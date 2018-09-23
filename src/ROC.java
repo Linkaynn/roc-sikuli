0e3a1411b81c5a5de6427da461694615bf88cc8f
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2018-09-23 14:23:26 +0200

    Added controlsfx and Move class

--- src/ROC.java
+++ src/ROC.java
@@ -1,5 +1,18 @@
-public class ROC {
-    public static void main(String[] args) {
-	    
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
+import movement.Move;
+import org.sikuli.script.FindFailed;
+
+public class ROC extends Application{
+	public static void main(String[] args) {
+		Application.launch(ROC.class);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws FindFailed {
+		Move.up();
+		Move.down();
+		Move.left();
+		Move.right();
+	}
 }
---
+++ src/movement/Move.java
@@ -0,0 +1,36 @@
+package movement;
+
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Location;
+import org.sikuli.script.Screen;
+
+import java.awt.*;
+
+public class Move {
+	private static Screen PRIMARY_SCREEN = Screen.getPrimaryScreen();
+
+	private static Location calculateMiddle() {
+		Rectangle bounds = Screen.getBounds(0);
+		return new Location(bounds.width / 2, bounds.height / 2);
+	}
+
+	public static void up() throws FindFailed {
+		Location middle = calculateMiddle();
+		PRIMARY_SCREEN.dragDrop(new Location(middle.x, middle.y / 2), middle);
+	}
+
+	public static void down() throws FindFailed {
+		Location middle = calculateMiddle();
+		PRIMARY_SCREEN.dragDrop(middle, new Location(middle.x, middle.y + middle.y / 2));
+	}
+
+	public static void left() throws FindFailed {
+		Location middle = calculateMiddle();
+		PRIMARY_SCREEN.dragDrop(new Location(middle.x / 2, middle.y), middle);
+	}
+
+	public static void right() throws FindFailed {
+		Location middle = calculateMiddle();
+		PRIMARY_SCREEN.dragDrop(middle, new Location(middle.x +  middle.x / 2, middle.y));
+	}
+}
