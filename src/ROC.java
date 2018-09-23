ffcff4c0adb802bcfb84619b69cd711d91969460
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2018-09-23 19:18:29 +0200

    Minor change

--- src/ROC.java
+++ src/ROC.java
@@ -2,15 +2,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import settings.Settings;
 
-import java.io.IOException;
-
 public class ROC extends Application{
 	public static void main(String[] args) {
 		Application.launch(ROC.class);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws IOException {
+	public void start(Stage primaryStage) {
 		Settings.initialize();
 	}
 }
